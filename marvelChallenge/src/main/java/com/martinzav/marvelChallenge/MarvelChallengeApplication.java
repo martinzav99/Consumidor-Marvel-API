1016022c7d31f999e77d10960417437c8d355ce2
Martin <martinkapo99@gmail.com>
2024-01-09 16:44:32 -0300

    initilizer + profiles + compose with bbdd for test only

---
+++ marvelChallenge/src/main/java/com/martinzav/marvelChallenge/MarvelChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.martinzav.marvelChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelChallengeApplication.class, args);
+	}
+
+}
---
+++ marvelChallenge/src/test/java/com/martinzav/marvelChallenge/MarvelChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinzav.marvelChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
